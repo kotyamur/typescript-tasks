3bc26f7bd3434c49585250a64635508fa17ad9d0
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-19 18:39:35 +0300

    Update 1basic.ts

--- src/baseTypes/1basic.ts
+++ src/baseTypes/1basic.ts
@@ -1,4 +1,6 @@
-// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінни
+
+/////////////////////////////////////////////basic
 
 //number
 let age: number = 50;
@@ -15,5 +17,58 @@ let notInitialize: undefined;
 let callback = (a: number) => {
   return 100 + a;
 };
+
+///////////////////////////////////////////////Складні типи
+
+//////////////////////////////////////////////////////////object
+const obj: object = {}; //const obj: {} = {};
+
+let user: { name: string; age: number } = {
+  name: "Tom",
+  age: 30,
+};
+// more comfortable type or interface
+type User = {
+  name: string;
+  age: number;
+};
+// or
+interface User1 {
+  name: string;
+  age: number;
+}
+
+let user2: User = {
+  name: "Tom",
+  age: 30,
+};
+
+/////////////////////////////////////////////////Array
+
+let arrString: string[] = ["Text", "Text"];
+
+let arrNumber: number[] = [3, 1];
+
+let matrix: number[][] = [
+  [1, 2],
+  [3, 4],
+];
+
+let mixed: (number | string)[] = [1, "two"];
+//generic
+let numbers: Array<number> = [1, 2, 3, 4, 5];
+
+type User2 = {
+  name: string;
+  age: number;
+};
+
+let users: User2[] = [
+  { name: "Tom", age: 30 },
+  { name: "Jack", age: 25 },
+  { name: "Alice", age: 32 },
+];
+
+let arrAny: any[] = [123, "TEXT", { name: "Tom" }, [1, 2, 3]];
 
 export { age, name, toggle, empty, notInitialize, callback };
